cf650d482aabd139fe153b9e10d3e777a20a742a
developwithpranjal <pranjalbros123@gmail.com>
2026-01-06 17:56:43 +0530

    completed

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let steps = document.querySelectorAll(".details");
 let form = document.querySelector(".RegForm");
 let nextbtn = document.querySelectorAll(".nextbtn");
-let submitbtn = document.querySelector(".submitbtn")
+let submitbtn = document.querySelector(".submitbtn");
+let result = document.querySelector(".result");
 let currentStep = 0;
 
 function showdetails(index) {
   steps.forEach((det) => det.classList.remove("active"));
   steps[index].classList.add("active");
-
-//   console.log(index);
 }
 
 nextbtn.forEach((btn) => {
@@ -19,4 +18,30 @@ nextbtn.forEach((btn) => {
       showdetails(currentStep);
     }
   });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let output = {};
+
+  console.log(output);
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].classList.contains("nextbtn")) {
+      continue;
+    } else {
+      output[form.elements[i].name] = form.elements[i].value;
+    }
+  }
+  let text = document.createElement("div");
+  form.style.display = "none"
+  result.style.display = "block" 
+
+  for (let x in output) {
+    let p = document.createElement("p");
+    p.textContent = `${x} : ${output[x]}`
+    text.append(p);
+     
+  }
+  result.append(text);
+
 });
